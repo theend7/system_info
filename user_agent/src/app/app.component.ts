34cc90207a2b457c53a3804e38863e572baa0fd1
Darko Vulicevic <darko.vulicevic@enreach.com>
2023-12-03 18:31:39 +0100

    express init

--- user_agent/src/app/app.component.ts
+++ user_agent/src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'user_agent';
+
+    constructor() {
+        return;
+    }
+
+    public ui = {
+        os: '',
+        broswer: '',
+    }
 }
